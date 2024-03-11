bf293f1c3139053d27ee0be41ef23e48fae21f16
Scopel_Yan <devscopelyan@gmail.com>
2024-03-11 12:39:19 -0300

    Melhoramento-visivel-dos numeros randomicos

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 let button = document.querySelector("#Sorteador-button")
 let quantity = document.querySelector("#quantity-sorts")
+let showNumber = document.querySelectorAll('.container-result')
 
 //Função de sorteio
 function sortear(minimo, maximo) {
   minimo = Math.ceil(minimo);//Define o valor minimo da funcão arredondando para cima
   maximo = Math.floor(maximo);//Define o valor maximo da funcão arredondando para baixo
 
+  showNumber.forEach(function (acontece) {
+    acontece.textContent = ""
+  })
   //Faz com que o input gerador de números randomico funcione
-  for(let i = 0; i < quantity.value; i++){
+  for (let i = 0; i < quantity.value; i++) {
     let result = Math.floor(Math.random() * (maximo - minimo + 1)) + minimo;
-    alert('Número ' + (i + 1) + ': ' + result)
+    // alert('Número ' + (i + 1) + ': ' + result)
+    if ( i < showNumber.length){
+      showNumber[i].textContent = result
+      showNumber[i].style.display = 'inline-block'
+    }
   }
+  
 }
 
 //Função onde o e feito o sorteio através do botão
@@ -24,7 +33,7 @@ function realizarSorteio() {
     alert("Por favor, insira valores válidos. O primeiro campo deve ser menor que o segundo campo.");
     return;
   }
-  var resultadoSorteio = sortear(minimo, maximo); 
+  var resultadoSorteio = sortear(minimo, maximo);
 }
 
 function limitarCaracteres(input, limite) {//Limita o tanto de caracteres permitidos
@@ -32,4 +41,17 @@ function limitarCaracteres(input, limite) {//Limita o tanto de caracteres permit
   if (texto.length > limite) {
     input.value = texto.substring(0, limite);
   }
+  if (quantity.value > 10) {
+    quantity.value = 10
+  }
 }
+function muda(){
+  o = 10
+  for(i = 0; o > i; i++ ){
+    showNumber[i].style.display = 'none'
+  }
+
+  
+}
+quantity.addEventListener('change', muda)
+
